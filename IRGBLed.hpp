099320a1a14cf5bd40134f9fc053ba4d9f04f9c5
elominp <guillaume.pirou@epitech.eu>
2017-12-19 11:30:47 +0100

    Remove still unused RGBLed.hpp, will be specialized in two classes for common cathode and common anode leds. Create an interface for RGB leds named IRGBLed

---
+++ IRGBLed.hpp
@@ -0,0 +1,25 @@
+#ifndef IRGBLED_HPP
+# define IRGBLED_HPP
+
+# include <stdint.h>
+# include "IDisplay.hpp"
+
+namespace woodBox {
+	namespace display {
+		class IRGBLed : public IDisplay {
+			public:
+				struct Color {
+					uint8_t red;
+					uint8_t green;
+					uint8_t blue;
+				};
+				virtual ~IDisplay() = 0;
+                virtual void clear() = 0;
+                virtual void update() = 0;
+				virtual const Color &getColor() = 0;
+				virtual void setColor(const Color &) = 0;
+		};
+	}
+}
+
+#endif /* IRGBLED_HPP */
