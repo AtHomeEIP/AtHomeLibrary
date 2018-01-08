e23f879b78cda0f6d6119733eff74042dfb81316
elominp <guillaume.pirou@epitech.eu>
2018-01-08 15:00:24 +0100

    WoodBox is now usable as a full fledged arduino library, yaygit add .! :)

---
+++ woodBox.h
@@ -0,0 +1,20 @@
+#ifndef WOODBOX_H
+# define WOODBOX_H
+
+# include "AWoodBoxModule.hpp"
+# include "Capacitor.hpp"
+# include "CommandEnumerate.hpp"
+# include "CommandProfile.hpp"
+# include "CommandSenderHelper.hpp"
+# include "CommandSetEndPoint.hpp"
+# include "CommandSetWiFi.hpp"
+# include "CommandSyncTime.hpp"
+# include "CommandUploadData.hpp"
+# include "EInk.hpp"
+# include "ESP8266WiFiCommunicator.hpp"
+# include "GroveChainableLED.hpp"
+# include "LithiumBattery.hpp"
+# include "MQ2.hpp"
+# include "NeoPixel.hpp"
+
+#endif /* WOODBOX_H */
