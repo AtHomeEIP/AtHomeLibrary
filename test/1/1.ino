45e49e44675fa6a9a6aff2271be7a1c9323a1586
elominp <guillaume.pirou@epitech.eu>
2018-01-21 17:21:30 +0000

    put the updated test sketch

--- test/1/1.ino
+++ test/1/1.ino
@@ -1,66 +1,7 @@
 #include <string.h>
 #include <woodBox.h>
 
-class MyModule : public woodBox::module::AWoodBoxModule<uint16_t, 15> {
-  public:
-    MyModule():AWoodBoxModule() {}
-    virtual ~MyModule() {}
-  protected:
-                virtual void onSampleSensor() {}
-                virtual void onUpdateDisplay() {}
-};
-
 #ifdef __AVR__
-
-/* AVRGCC Stack monitoring */
-// From: http://www.avrfreaks.net/forum/soft-c-avrgcc-monitoring-stack-usage?name=PNphpBB2&file=viewtopic&t=52249
-/*extern "C" {
-extern uint8_t _end;
-extern uint8_t __stack;
-//void StackPaint(void) __attribute__ ((naked)) __attribute__ ((section (".init1")));
-
-void StackPaint(void)
-{
-#if 1
-    uint8_t *p = &_end;
-
-    while(p <= &__stack)
-    {
-        *p = 0x4e;
-        p++;
-    }
-#else
-    __asm volatile ("    ldi r30,lo8(_end)\n"
-                    "    ldi r31,hi8(_end)\n"
-                    "    ldi r24,lo8(0x4e)\n" // STACK_CANARY = 0xc5
-                    "    ldi r25,hi8(__stack)\n"
-                    "    rjmp .cmp\n"
-                    ".loop:\n"
-                    "    st Z+,r24\n"
-                    ".cmp:\n"
-                    "    cpi r30,lo8(__stack)\n"
-                    "    cpc r31,r25\n"
-                    "    brlo .loop\n"
-                    "    breq .loop"::);
-#endif
-}
-
-uint16_t StackCount(void)
-{
-    const uint8_t *p = &_end;
-    uint16_t       c = 0;
-
-    while(p <= &__stack)
-    {
-        if (*p == 0x4e)
-            c++;
-        p++;
-    }
-
-    return c;
-}
-
-}*/
 
 int freeRam () {
   extern int __heap_start, *__brkval; 
@@ -70,7 +11,7 @@ int freeRam () {
 
 #endif
 
-MyModule *me = MyModule::getInstance<MyModule>();
+woodBox::module::WoodBoxModule<uint16_t, 15> *me = woodBox::module::WoodBoxModule<uint16_t, 15>::getInstance();
 woodBox::communication::wifi::ESP8266WiFiCommunicator wifi_com(2, 3);
 woodBox::communication::commands::CommandSetWiFi set_wifi(wifi_com);
 
@@ -79,7 +20,6 @@ const PROGMEM char password[] = "HelloWorld";
 
 void setup() {
   // put your setup code here, to run once:
-    //StackPaint();
     Serial.begin(9600);
     delay(1);
     Serial.println(F("Setup..."));
@@ -93,18 +33,11 @@ void setup() {
     ap.channel = 5;
     ap.band = wifi_frequency_band::TWO_POINT_FOUR_GHZ;
 #ifdef __AVR__
-    /*if (&_end >= &__stack) {
-      Serial.print(F("Stack overflow: "));
-      Serial.println((size_t)(&_end - &__stack));
-    }
-    else
-        Serial.println(StackCount(), DEC);*/
     Serial.println(freeRam());
     Serial.flush();
 #endif
-    //wifi_com.setAccessPoint(ap);
-    //wifi_com.open();
-    //wifi_com.connect();
+    wifi_com.setAccessPoint(ap);
+    wifi_com.connect();
 }
 
 void loop() {
