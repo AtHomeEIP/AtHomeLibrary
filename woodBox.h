1a0ad935241f486f1f30d9b5ad53bcbdc48e7438
elominp <guillaume.pirou@epitech.eu>
2018-01-28 18:06:41 +0000

    disable WoodBoxWiFiModule for MSP430 targets, the ArduinoJSON library doesn't compile (but it should, it's officially supported on their github. Nevertheless, even examples don't compile on MSP430, need to open a issue) unti it's fixed

--- woodBox.h
+++ woodBox.h
@@ -1,30 +1,34 @@
 #ifndef WOODBOX_H
 # define WOODBOX_H
 
-# include "WoodBoxWiFiModule.hpp"
 # include "WoodBoxModule.hpp"
 # include "network_ip_types.hpp"
 # include "wifi_types.hpp"
 # ifdef ARDUINO
 #  include "GroveChainableLED.hpp"
-# endif
+# endif /* ARDUINO */
+# ifndef __MSP430__
+# include "WoodBoxWiFiModule.hpp"
 # include "ESP8266WiFiCommunicator.hpp"
+# endif /* __MSP430__ */
 # include "AnalogSensor.hpp"
 # include "CommonCathodeRGBLed.hpp"
 
 # if defined(__AVR__) && defined(ARDUINO)
 #  include "ArduinoEEPROM.hpp"
 using woodBox::storage::ArduinoEEPROM;
-# endif
+# endif /* __AVR__ && ARDUINO */
 
 using woodBox::module::WoodBoxModule;
+# ifndef __MSP430__
 using woodBox::module::WoodBoxWiFiModule;
 using woodBox::communication::wifi::ESP8266WiFiCommunicator;
+# endif /* __MSP430__ */
 using woodBox::sensor::AnalogSensor;
 using woodBox::display::CommonCathodeRGBLed;
 
 # ifdef ARDUINO
 using woodBox::display::GroveChainableLED;
-# endif
+# endif /* ARDUINO */
 
 #endif /* WOODBOX_H */
