39ec3f32e874293da12ca57371c7774da4896466
elominp <guillaume.pirou@epitech.eu>
2018-01-28 16:31:51 +0000

    regen doc

--- doc/html/search/classes_2.js
+++ doc/html/search/classes_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['color',['Color',['../structwood_box_1_1display_1_1_a_r_g_b_led_1_1_color.html',1,'woodBox::display::ARGBLed']]]
+  ['color',['Color',['../structwood_box_1_1display_1_1_a_r_g_b_led_1_1_color.html',1,'woodBox::display::ARGBLed']]],
+  ['commoncathodergbled',['CommonCathodeRGBLed',['../classwood_box_1_1display_1_1_common_cathode_r_g_b_led.html',1,'woodBox::display']]]
 ];
