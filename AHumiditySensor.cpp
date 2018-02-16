634e6949d93dfa32b8a21e86cc35f5888b314e77
elominp <guillaume.pirou@epitech.eu>
2018-02-16 17:47:28 +0000

    implement humidity estimation

--- AHumiditySensor.cpp
+++ AHumiditySensor.cpp
@@ -6,9 +6,23 @@ namespace athome {
 
         AHumiditySensor::~AHumiditySensor() {}
 
+        /**
+         * Return an estimation of the humidity inside the room
+         *
+         * Values are from https://www.jechange.fr/energie/duale/guides/thermostat-temperature-4101
+         * @return estimation of humidity quality on a scale from 1 (worst) to 10 (best)
+         */
         ISensor::ISensorScale AHumiditySensor::getEstimate() {
-            // TODO: What are the values???
-            return ISensor::ISensorScale::ZERO;
+            int humidity = getLastSample();
+            if (humidity < 30 || humidity > 65) {
+                return ISensor::ISensorScale::ONE;
+            }
+            else if (humidity >= 50 && humidity <= 55) {
+                return ISensor::ISensorScale::TEN;
+            }
+            else {
+                return ISensor::ISensorScale::FIVE;
+            }
         }
     }
 }
