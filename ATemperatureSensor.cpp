7acb62259cd281523cc227ca1b0b9ea015c16f9d
elominp <guillaume.pirou@epitech.eu>
2018-02-16 17:44:39 +0000

    implement temperature estimation

--- ATemperatureSensor.cpp
+++ ATemperatureSensor.cpp
@@ -6,9 +6,23 @@ namespace athome {
 
         ATemperatureSensor::~ATemperatureSensor() {}
 
+        /**
+         * Return an estimation of the quality of the temperature
+         *
+         * Values are from: https://www.jechange.fr/energie/duale/guides/thermostat-temperature-4101
+         * @return estimation of temperature on a scale from 1 (worst) to 10 (best)
+         */
         ISensor::ISensorScale ATemperatureSensor::getEstimate() {
-            // TODO: What are the values???
-            return ISensor::ISensorScale::ZERO;
+            int temp = getLastSample();
+            if (temp < 16) {
+                return ISensor::ISensorScale::ONE;
+            }
+            else if (temp >= 16 && temp <= 18) {
+                return ISensor::ISensorScale::FOUR;
+            }
+            else {
+                return ISensor::ISensorScale::TEN;
+            }
         }
     }
 }
