1807d91eee17abe8c54bd2df07adacf69d17bb03
Guillaume Pirou <pirou_g@epitech.eu>
2018-03-13 10:42:58 +0000

    add minimum example using the dummy sensor to generate random values

---
+++ examples/HelloWorld/HelloWorld.ino
@@ -0,0 +1,18 @@
+#include <AtHome.h>
+
+using SimplestModule = AtHomeModule<uint16_t, 15>;
+
+Stream *streams[] = {&Serial, nullptr};
+DummySensor<ALuminositySensor, uint16_t, 50000> dummy;
+SimplestModule *module = SimplestModule::getInstance();
+
+void setup() {
+    Serial.begin(115200);
+    module->setStreams(streams);
+    module->setSensor(&dummy);
+    module->setup();
+}
+
+void run() {
+    module->run();
+}
