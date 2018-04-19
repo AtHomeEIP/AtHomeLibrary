9bb537a29d505275e8cba87399bbb22cfd7f55a4
Guillaume Pirou <pirou_g@epitech.eu>
2018-04-19 12:27:33 +0200

    protect SoundSensor and add dynamic choice of pin

--- SoundSensor.hpp
+++ SoundSensor.hpp
@@ -1,18 +1,28 @@
-#include <ISensor.hpp>
+#ifndef SOUNDSENSOR_HPP
+# define SOUNDSENSOR_HPP
+
+# include <ISensor.hpp>
+
+# ifdef ARDUINO
 
 namespace athome {
   namespace sensor {
     class SoundSensor : public ISensor {
     public:
-      uint8_t *getSample(){
-        sampleValue = digitalRead(8);
-        return reinterpret_cast<uint8_t *>(sampleValue);
-      }
-      ISensorScale getEstimate(){
-       return (sampleValue) ? ISensorScale::TEN : ISensorScale::ONE;
-     }
-   private:
-      bool sampleValue;
+      SoundSensor(int pin):_pin(pin),_sampleValue(false) {}
+      SoundSensor(const SoundSensor &) = delete; // Has no meaning
+      SoundSensor &operator=(const SoundSensor &) = delete; // Has no meaning
+      ~SoundSensor() {}
+      uint8_t *getSample(){ return reinterpret_cast<uint8_t *>((sampleValue = digitalRead(_pin))); }
+      ISensorScale getEstimate(){ return (_sampleValue) ? ISensorScale::TEN : ISensorScale::ONE; }
+      void setPin(int pin) { _pin = pin; }
+      int getPin() const { return _pin; }
+    private:
+      int _pin;
+      bool _sampleValue;
     };
   }
 }
+
+# endif /* ARDUINO */
+#endif /* SOUNDSENSOR_HPP */
