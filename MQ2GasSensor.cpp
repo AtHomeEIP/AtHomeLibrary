965809181143de504d6a1dd078437263adbad736
Guillaume Pirou <pirou_g@epitech.eu>
2018-04-22 17:15:44 +0200

    fix documentation format

--- MQ2GasSensor.cpp
+++ MQ2GasSensor.cpp
@@ -30,49 +30,57 @@ namespace athome {
             return _pin;
         }
 
-        /******************************* GetLPG **********************************************
-        * Only Possible when getValue is called /!\
-        * @return value of LPG contain in array_value[0]
-        */
+        /**
+         * Only Possible when getValue is called /!\
+         *
+         * @return value of LPG contain in a struct (struct.lpg)
+         */
         int MQ2GasSensor::getLPG() const {
             return _values.lpg;
         }
 
-        /******************************* GetCO **********************************************
-        * Only Possible when getValue is called /!\
-        * @return value of CO contain in array_value[1]
-        */
+        /**
+         * Only Possible when getValue is called /!\
+         *
+         * @return value of CO contain in a struct (struct.co)
+         */
         int MQ2GasSensor::getCO() const {
             return _values.co;
         }
 
-        /******************************* GetSMOKE **********************************************
-        * Only Possible when getValue is called /!\
-        * @return value of SMOKE contain in array_value[2]
-        */
+        /**
+         * Only Possible when getValue is called /!\
+         *
+         * @return value of SMOKE contain in a struct (struct.smoke)
+         */
         int MQ2GasSensor::getSMOKE() const {
             return _values.smoke;
         }
 
-        /****************** MQResistanceCalculation ****************************************
-        *    Input:   raw_adc - raw value read from adc, which represents the voltage
-        *    Output:  the calculated sensor resistance
-        *    Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
-        *    across the load resistor and its resistance, the resistance of the sensor
-        *    could be derived.
-        ************************************************************************************/
+        /**
+         * Input:   raw_adc - raw value read from adc, which represents the voltage
+         *
+         * Output:  the calculated sensor resistance
+         *
+         * Remarks: The sensor and the load resistor forms a voltage divider.
+         *
+         * Given the voltage across the load resistor and its resistance, the resistance of the sensor
+         * could be derived.
+         */
         float MQ2GasSensor::MQResistanceCalculation(int raw_adc) {
             return ((static_cast<float>RL_value*(1023-raw_adc)/raw_adc));
         }
 
-        /***************************** MQCalibration ****************************************
-        *    Input:   mq_pin - analog channel
-        *    Output:  Ro of the sensor
-        *    Remarks: This function assumes that the sensor is in clean air. It use
-        *             MQResistanceCalculation to calculates the sensor resistance in clean air
-        *             and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about
-        *             10, which differs slightly between different sensors.
-        ************************************************************************************/
+        /**
+         * Input:   mq_pin - analog channel
+         *
+         * Output:  Ro of the sensor
+         *
+         * Remarks: This function assumes that the sensor is in clean air.
+         * It use MQResistanceCalculation to calculates the sensor resistance in clean air
+         * and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about
+         * 10, which differs slightly between different sensors.
+         */
         float MQ2GasSensor::MQCalibration(int mqpin) {
             float val = 0;
 
@@ -85,14 +93,19 @@ namespace athome {
             return val;
         }
 
-        /*****************************  MQRead *********************************************
-        * Input:   mq_pin - analog channel
-        * Output:  Rs of the sensor
-        * Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
-        *          The Rs changes as the sensor is in the different consentration of the target
-        *          gas. The sample times and the time interval between samples could be configured
-        *          by changing the definition of the macros.
-        ************************************************************************************/
+        /**
+         * Input:   mq_pin - analog channel
+         *
+         * Output:  Rs of the sensor
+         *
+         * Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
+         *
+         * The Rs changes as the sensor is in the different consentration of the target
+         * gas.
+         *
+         * The sample times and the time interval between samples could be configured
+         * by changing the definition of the macros.
+         */
         float MQ2GasSensor::MQRead(int mq_pin) {
             float   rs = 0;
 
@@ -104,13 +117,16 @@ namespace athome {
             return rs;
         }
 
-        /*****************************  MQGetGasPercentage **********************************
-        * Input:   rs_ro_ratio - Rs divided by Ro
-        *  gas_id      - target gas type
-        * Output:  ppm of the target gas
-        * Remarks: This function passes different curves to the MQGetPercentage function which
-        *  calculates the ppm (parts per million) of the target gas.
-        ************************************************************************************/
+        /**
+         * Input:   rs_ro_ratio: Rs divided by Ro gas_id
+         *
+         * target gas type
+         *
+         * Output:  ppm of the target gas
+         *
+         * Remarks: This function passes different curves to the MQGetPercentage function which
+         * calculates the ppm (parts per million) of the target gas.
+         */
         int MQ2GasSensor::MQGetGasPercentage(float rs_ro_ratio, int gas_id) {
             if (gas_id == GAS_LPG){
                 return MQGetPercentage(rs_ro_ratio, const_cast<float *>(_LPGCurve));
@@ -124,15 +140,18 @@ namespace athome {
             return 0;
         }
 
-        /*****************************  MQGetPercentage **********************************
-        * Input:   rs_ro_ratio - Rs divided by Ro
-        *  pcurve      - pointer to the curve of the target gas
-        * Output:  ppm of the target gas
-        * Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm)
-        *          of the line could be derived if y(rs_ro_ratio) is provided. As it is a
-        *          logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic
-        *          value.
-        ************************************************************************************/
+        /**
+         * Input:   rs_ro_ratio: Rs divided by Ro pcurve
+         *
+         * pointer to the curve of the target gas
+         *
+         * Output:  ppm of the target gas
+         *
+         * Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm)
+         * of the line could be derived if y(rs_ro_ratio) is provided. As it is a
+         * logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic
+         * value.
+         */
         int MQ2GasSensor::MQGetPercentage(float rs_ro_ratio, float *pcurve) {
             return static_cast<int>(pow(10, ((log(rs_ro_ratio) - pcurve[1]) / pcurve[2]) + pcurve[0]));
         }
