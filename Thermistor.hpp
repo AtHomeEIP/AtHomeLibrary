0d422806eb56e1779d7d6333732290f8ca814628
Guillaume Pirou (@Elominp) <guillaume.pirou@epitech.eu>
2018-04-28 16:42:46 +0200

    convert thermistor code to use our own math functions

--- Thermistor.hpp
+++ Thermistor.hpp
@@ -1,8 +1,8 @@
 #ifndef THERMISTOR_HPP
 # define THERMISTOR_HPP
 
-# include <math.h>
 # include "ATemperatureSensor.hpp"
+# include "Maths.hpp"
 
 # ifdef ARDUINO
 #  include <Arduino.h>
@@ -22,20 +22,7 @@ namespace athome {
         template <uint32_t R1, uint32_t R2, uint32_t R3, size_t PREC = 1024, uint8_t VREF = 5, uint32_t RREF = 10000>
         class Thermistor : public ATemperatureSensor {
         public:
-            Thermistor(int pin):_pin(pin),_sample(NAN),_increment(5 / PREC) {
-                float l1 = log(R1);
-                float l2 = log(R2);
-                float l3 = log(R3);
-                float y1 = 1. / 5.;
-                float y2 = 1. / 25.;
-                float y3 = 1. / 50.;
-                float lambda2 = (y2 - y1) / (l2 - l1);
-                float lambda3 = (y3 - y1) / (l3 - l1);
-
-                _c = ((lambda3 - lambda2) / (l3 - l2)) * (1 / (l1 + l2 + l3));
-                _b = lambda2 - (_c * (pow(l1, 2) + (l1 * l2) + pow(l2, 2)));
-                _a = y1 - ((_b + (pow(l1, 2) * _c)) * l1);
-            }
+            Thermistor(int pin):_pin(pin),_sample(NAN),_increment(5 / PREC) {}
             Thermistor(const Thermistor &) = delete;
             Thermistor &operator=(const Thermistor &) = delete;
             ~Thermistor() {}
@@ -47,7 +34,7 @@ namespace athome {
 # endif /* ARDUINO */
                 float voltage = adc * _increment;
                 float resistance = voltage * RREF / (VREF - voltage);
-                _sample = (1 / (_a + (_b * log(resistance)) + (_c * pow(log(resistance), 3)))) - 273.15;
+                _sample = (1 / (_a + (_b * utility::math::log<float>(resistance)) + (_c * utility::math::pow<float>(utility::math::log<float>(resistance), 3)))) - 273.15;
 
                 return reinterpret_cast<uint8_t *>(&_sample);
             }
@@ -58,9 +45,17 @@ namespace athome {
             float                   _sample;
             float                   _increment;
         private:
-            float  _c;
-            float  _b;
-            float  _a;
+            static constexpr float  _l1 = utility::math::static_log<float>(R1);
+            static constexpr float  _l2 = utility::math::static_log<float>(R2);
+            static constexpr float  _l3 = utility::math::static_log<float>(R3);
+            static constexpr float  _y1 = 1. / 5.;
+            static constexpr float  _y2 = 1. / 25.;
+            static constexpr float  _y3 = 1. / 50.;
+            static constexpr float  _lambda2 = (_y2 - _y1) / (_l2 - _l1);
+            static constexpr float  _lambda3 = (_y3 - _y1) / (_l3 - _l1);
+            static constexpr float  _c = ((_lambda3 - _lambda2) / (_l3 - _l2)) * (1 / (_l1 + _l2 + _l3));
+            static constexpr float  _b = _lambda2 - (_c * (utility::math::static_pow<float>(_l1, 2) + (_l1 * _l2) + utility::math::static_pow<float>(_l2, 2)));
+            static constexpr float  _a = _y1 - ((_b + (utility::math::static_pow<float>(_l1, 2) * _c)) * _l1);
         };
     }
 }
