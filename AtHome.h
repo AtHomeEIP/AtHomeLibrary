3df5b12ad962f0c2a88b09f8b9fae3bd11169150
Guillaume Pirou (@Elominp) <guillaume.pirou@epitech.eu>
2018-05-13 16:26:43 +0100

    add a config header to modularize framework and refactor AtHome.h

--- AtHome.h
+++ AtHome.h
@@ -1,91 +1,127 @@
 #ifndef ATHOME_H
 # define ATHOME_H
 
-# include "AtHomeModule.hpp"
-# include "NetworkIPTypes.hpp"
-# include "WiFiTypes.hpp"
-# include "LDRLuminositySensor.hpp"
-# ifdef ARDUINO
-#  include "GroveChainableLED.hpp"
-#  include "SoundSensor.hpp"
-#  include "MQ2GasSensor.hpp"
-# endif /* ARDUINO */
-# ifndef __MSP430__
-# include "AtHomeWiFiModule.hpp"
-# include "ESP8266WiFiCommunicator.hpp"
-# endif /* __MSP430__ */
-# include "AnalogSensor.hpp"
-# include "CommonCathodeRGBLed.hpp"
-# include "CommonAnodeRGBLed.hpp"
-# if defined(__AVR__) && defined(ARDUINO)
-#  include "ArduinoEEPROM.hpp"
-# endif /* __AVR__ && ARDUINO */
-# ifdef ARDUINO_AVR_UNO
-#  include "GroveAirQualitySensor.hpp"
-# endif /* ARDUINO_AVR_UNO */
-# if defined(ARDUINO) && (defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__) ||\
-     defined(__AVR_ATmega2560__))
-#  include "GroveDigitalLightSensor.hpp"
-# endif /* defined(ARDUINO) && (defined(__ATmega328__) || defined(__ATmega32U4__) || defined(__ATmega2560__)) */
-# if !defined(__MSP430__) && !defined(TARGET_IS_MSP432P4XX) && !defined(__PIC32MX__)
-#  include "NeoPixel.hpp"
-# endif /* List of incompatible architectures */
-# include "AHumiditySensor.hpp"
-# include "ATemperatureSensor.hpp"
-# include "ALuminositySensor.hpp"
-# include "ANoiseSensor.hpp"
-# include "AAirQualitySensor.hpp"
-# include "DummySensor.hpp"
-# include "MonochromaticLED.hpp"
-# include "PWMLED.hpp"
-# include "Thermistor.hpp"
-# include "Maths.hpp"
-# include "TMP36GZTemperatureSensor.hpp"
-# ifdef __MSP430__
-#  include "MSP430FRAM.hpp"
+# ifndef ARDUINO
+#  error Bare metal is not yet supported
 # endif
 
+# include "AtHomeConfig.h"
+# include "AtHomeModule.hpp"
+# include "Maths.hpp"
+
 using athome::module::AtHomeModule;
-# ifndef __MSP430__
-using athome::module::AtHomeWiFiModule;
-using athome::communication::wifi::ESP8266WiFiCommunicator;
-# endif /* __MSP430__ */
-using athome::sensor::AnalogSensor;
-using athome::display::CommonCathodeRGBLed;
-using athome::display::CommonAnodeRGBLed;
+
+# ifndef DISABLE_SENSOR
+#  ifndef DISABLE_HUMIDITY_SENSOR
+#   include "AHumiditySensor.hpp"
 using athome::sensor::AHumiditySensor;
+#  endif /* DISABLE_HUMIDITY_SENSOR */
+#  ifndef DISABLE_TEMPERATURE_SENSOR
+#   include "ATemperatureSensor.hpp"
 using athome::sensor::ATemperatureSensor;
-using athome::sensor::ALuminositySensor;
-using athome::sensor::ANoiseSensor;
-using athome::sensor::AAirQualitySensor;
-using athome::sensor::DummySensor;
-using athome::display::MonochromaticLED;
-using athome::display::PWMLED;
+#   ifndef DISABLE_THERMISTOR_SENSOR
+#    include "Thermistor.hpp"
 using athome::sensor::Thermistor;
+#   endif /* DISABLE_THERMISTOR_SENSOR */
+#   ifndef DISABLE_TMP36GZ_TEMPERATURE_SENSOR
+#    include "TMP36GZTemperatureSensor.hpp"
 using TMP36GZTemperatureSensor5V = athome::sensor::TMP36GZTemperatureSensor<5000000, 10>;
 using TMP36GZTemperatureSensor3V3 = athome::sensor::TMP36GZTemperatureSensor<3300000, 10>;
+#   endif /* DISABLE_TMP36GZ_TEMPERATURE_SENSOR */
+#  endif /* DISABLE_TEMPERATURE_SENSOR */
+#  ifndef DISABLE_LUMINOSITY_SENSOR
+#   include "ALuminositySensor.hpp"
+using athome::sensor::ALuminositySensor;
+#   if !defined(DISABLE_LDR_LUMINOSITY_SENSOR)
+#    include "LDRLuminositySensor.hpp"
 using LDRLuminositySensor5V = athome::sensor::LDRLuminositySensor<5000000, 10, 10000>;
 using LDRLuminositySensor3V3 = athome::sensor::LDRLuminositySensor<3300000, 10, 10000>;
-# ifdef ARDUINO
+#   endif /* DISABLE_LDR_LUMINOSITY_SENSOR */
+#   if (defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__) ||
+       defined(__AVR_ATmega2560__)) && !defined(DISABLE_GROVE_DIGITAL_LIGHT_SENSOR)
+#    include "GroveDigitalLightSensor.hpp"
+        using athome::sensor::GroveDigitalLightSensor;
+#   endif /* defined(ARDUINO) && (defined(__ATmega328__) || defined(__ATmega32U4__) || defined(__ATmega2560__)) &&
+             !defined(DISABLE_GROVE_DIGITAL_LIGHT_SENSOR) */
+#  endif /* DISABLE_LUMINOSITY_SENSOR */
+#  ifndef DISABLE_NOISE_SENSOR
+#   include "ANoiseSensor.hpp"
+using athome::sensor::ANoiseSensor;
+#   ifndef DISABLE_SOUND_SENSOR
+#    include "SoundSensor.hpp"
 using athome::sensor::SoundSensor;
-using athome::display::GroveChainableLED;
-using athome::sensor::MQ2GasSensor;
-# endif /* ARDUINO */
-# if defined(ARDUINO) && (defined(__AVR_ATmega328__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__) ||\
-     defined(__AVR_ATmega2560__))
-using athome::sensor::GroveDigitalLightSensor;
-# endif /* defined(ARDUINO) && (defined(__ATmega328__) || defined(__ATmega32U4__) || defined(__ATmega2560__)) */
-# if !defined(__MSP430__) && !defined(TARGET_IS_MSP432P4XX) && !defined(__PIC32MX__)
-using athome::display::NeoPixel;
-# endif /* List of incompatible architectures */
-# if defined(__AVR__) && defined(ARDUINO)
-using athome::storage::ArduinoEEPROM;
-# endif /* __AVR__ && ARDUINO */
-# ifdef ARDUINO_AVR_UNO
+#   endif /* DISABLE_SOUND_SENSOR */
+#  endif /* DISABLE_NOISE_SENSOR */
+#  ifndef DISABLE_AIR_QUALITY_SENSOR
+#   include "AAirQualitySensor.hpp"
+using athome::sensor::AAirQualitySensor;
+#   if defined(ARDUINO_AVR_UNO) && !defined(DISABLE_GROVE_AIR_QUALITY_SENSOR)
+#    include "GroveAirQualitySensor.hpp"
 using athome::sensor::GroveAirQualitySensor;
-# endif /* ARDUINO_AVR_UNO */
-# ifdef __MSP430__
+#   endif /* ARDUINO_AVR_UNO && !DISABLE_GROVE_AIR_QUALITY_SENSOR */
+#   if !defined(DISABLE_MQ2_SENSOR)
+#    include "MQ2GasSensor.hpp"
+using athome::sensor::MQ2GasSensor;
+#   endif /* DISABLE_MQ2_SENSOR */
+#  endif /* DISABLE_AIR_QUALITY_SENSOR */
+#  ifndef DISABLE_DUMMY_SENSOR
+#   include "DummySensor.hpp"
+using athome::sensor::DummySensor;
+#  endif /* DISABLE_DUMMY_SENSOR */
+#  if !defined(DISABLE_ANALOG_SENSOR)
+#   include "AnalogSensor.hpp"
+using athome::sensor::AnalogSensor;
+#  endif /* DISABLE_ANALOG_SENSOR */
+# endif /* DISABLE_SENSOR */
+# ifndef DISABLE_DISPLAY
+#  ifndef DISABLE_MONOCHROMATIC_LED
+#   include "MonochromaticLED.hpp"
+using athome::display::MonochromaticLED;
+#  endif /* DISABLE_MONOCHROMATIC_LED */
+#  ifndef DISABLE_PWM_LED
+#   include "PWMLED.hpp"
+using athome::display::PWMLED;
+#  endif /* DISABLE_PWD_LED */
+#  if !defined(__MSP430__) && !defined(TARGET_IS_MSP432P4XX) && !defined(__PIC32MX__) && !defined(DISABLE_NEOPIXEL)
+#   include "NeoPixel.hpp"
+using athome::display::NeoPixel;
+#  endif /* List of incompatible architectures */
+#  if !defined(DISABLE_COMMON_ANODE_RGB_LED)
+#   include "CommonAnodeRGBLed.hpp"
+using athome::display::CommonAnodeRGBLed;
+#  endif /* DISABLE_COMMON_ANODE_RGB_LED */
+#  if !defined(DISABLE_COMMON_CATHODE_RGB_LED)
+#   include "CommonCathodeRGBLed.hpp"
+using athome::display::CommonCathodeRGBLed;
+#  endif /* DISABLE_COMMON_CATHODE_RGB_LED */
+#  if !defined(DISABLE_GROVE_CHAINABLE_LED)
+#   include "GroveChainableLED.hpp"
+using athome::display::GroveChainableLED;
+#  endif /* DISABLE_GROVE_CHAINABLE_LED */
+# endif /* DISABLE_DISPLAY */
+# ifndef DISABLE_COMMUNICATION
+#  ifndef DISABLE_NETWORK
+#   include "NetworkIPTypes.hpp"
+#   ifndef DISABLE_WIFI
+#    include "WiFiTypes.hpp"
+#    if !defined(__MSP430__) && !defined(DISABLE_ESP_AT_MODULE)
+#     include "AtHomeWiFiModule.hpp"
+#     include "ESP8266WiFiCommunicator.hpp"
+using athome::module::AtHomeWiFiModule;
+using athome::communication::wifi::ESP8266WiFiCommunicator;
+#    endif /* !defined(__MSP430__) && !defined(DISABLE_ESP_AT_MODULE) */
+#   endif /* DISABLE_WIFI */
+#  endif /* DISABLE_NETWORK */
+# endif /* DISABLE_COMMUNICATION */
+# ifndef DISABLE_PERSISTENT_STORAGE
+#  if defined(__MSP430__) && !defined(DISABLE_MSP430_FRAM)
+#   include "MSP430FRAM.hpp"
 using athome::storage::MSP430FRAM;
-# endif
+#  endif /* defined(__MSP430__) && !defined(DISABLE_MSP430_FRAM) */
+#  if defined(__AVR__) && !defined(DISABLE_AVR_EEPROM)
+#   include "ArduinoEEPROM.hpp"
+using athome::storage::ArduinoEEPROM;
+#  endif /* __AVR__ && !DISABLE_AVR_EEPROM */
+# endif /* DISABLE_PERSISTENT_STORAGE */
 
 #endif /* ATHOME_H */
---
+++ AtHomeConfig.h
@@ -0,0 +1,49 @@
+#ifndef ATHOMECONFIG_H
+# define ATHOMECONFIG_H
+
+// Macros to uncomment to disable full part of modules
+# define DISABLE_SENSOR
+# define DISABLE_DISPLAY
+# define DISABLE_PERSISTENT_STORAGE
+# define DISABLE_COMMUNICATION
+# define DISABLE_POWER_MANAGEMENT
+# define DISABLE_HEARTBEAT
+
+// Macros to uncomment to disable network communications. DISABLE_NETWORK disable all types of networks
+# define DISABLE_NETWORK
+# define DISABLE_WIFI
+
+// Macros to uncomment to disable evolved AtHomeModules
+# define DISABLE_ESP_AT_MODULE
+
+// Macros to uncomment to disable families of sensors
+# define DISABLE_AIR_QUALITY_SENSOR
+# define DISABLE_TEMPERATURE_SENSOR
+# define DISABLE_HUMIDITY_SENSOR
+# define DISABLE_NOISE_SENSOR
+# define DISABLE_LUMINOSITY_SENSOR
+
+// Macros to uncomment to disable sensors
+# define DISABLE_DUMMY_SENSOR
+# define DISABLE_LDR_LUMINOSITY_SENSOR
+# define DISABLE_SOUND_SENSOR
+# define DISABLE_MQ2_SENSOR
+# define DISABLE_ANALOG_SENSOR
+# define DISABLE_GROVE_AIR_QUALITY_SENSOR
+# define DISABLE_GROVE_DIGITAL_LIGHT_SENSOR
+# define DISABLE_THERMISTOR_SENSOR
+# define DISABLE_TMP36GZ_TEMPERATURE_SENSOR
+
+// Macros to uncomment to disable displays
+# define DISABLE_GROVE_CHAINABLE_LED
+# define DISABLE_COMMON_CATHODE_RGB_LED
+# define DISABLE_COMMON_ANODE_RGB_LED
+# define DISABLE_NEOPIXEL
+# define DISABLE_MONOCHROMATIC_LED
+# define DISABLE_PWM_LED
+
+// Macros to uncomment to disable persisent storages
+# define DISABLE_AVR_EEPROM
+# define DISABLE_MSP430_FRAM
+
+#endif /* ATHOMECONFIG_H */
