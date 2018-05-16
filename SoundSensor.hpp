573910246b865a689ba57f757d46d79511e6bba5
Guillaume Pirou (@Elominp) <guillaume.pirou@epitech.eu>
2018-05-16 14:25:27 +0100

    update SoundSensor

--- SoundSensor.hpp
+++ SoundSensor.hpp
@@ -7,22 +7,40 @@
 #  ifdef ARDUINO
 
 namespace athome {
-  namespace sensor {
-    class SoundSensor : public ISensor {
-    public:
-      SoundSensor(int pin):_pin(pin),_sampleValue(false) {}
-      SoundSensor(const SoundSensor &) = delete; // Has no meaning
-      SoundSensor &operator=(const SoundSensor &) = delete; // Has no meaning
-      ~SoundSensor() {}
-      uint8_t *getSample(){ return reinterpret_cast<uint8_t *>(&(_sampleValue = digitalRead(_pin))); }
-      ISensorScale getEstimate(){ return (_sampleValue) ? ISensorScale::TEN : ISensorScale::ONE; }
-      void setPin(int pin) { _pin = pin; }
-      int getPin() const { return _pin; }
-      int _pin;
-    private:
-      bool _sampleValue;
-    };
-  }
+    namespace sensor {
+        class SoundSensor : public ISensor {
+            public:
+                SoundSensor(int pin):
+                        _pin(pin),
+                        _sampleValue(false),
+                        _value({
+                                ISensorScale::ZERO,
+                                {
+                                    utility::units::UNIT::UNKNOWN,
+                                    utility::units::PREFIX::BASE_UNIT
+                                },
+                                reinterpret_cast<void *>(&_sampleValue),
+                                PSTR("Binary noise detector")
+                        }) {}
+                SoundSensor(const SoundSensor &) = delete; // Has no meaning
+                SoundSensor &operator=(const SoundSensor &) = delete; // Has no meaning
+                ~SoundSensor() {}
+
+                const ISensorValue &getSample() {
+                    _sampleValue = digitalRead(_pin);
+                    _value.estimate = (_sampleValue) ? ISensorScale::TEN : ISensorScale::ONE;
+                    return _value;
+                }
+                void setThresholds(const ISensorThresholds &thresholds) { (void)thresholds; }
+                void setPin(int pin) { _pin = pin; }
+                int getPin() const { return _pin; }
+
+            private:
+                int             _pin;
+                bool            _sampleValue;
+                ISensorValue    _value;
+        };
+    }
 }
 
 #  endif /* ARDUINO */
