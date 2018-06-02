0ea3cd84c8590727ebd4131425b37e8ae14c27b3
Guillaume Pirou (@Elominp) <guillaume.pirou@epitech.eu>
2018-06-02 12:12:05 +0100

    fix baudrate of gas module to 9600 like others

--- examples/GasModule/GasModule.ino
+++ examples/GasModule/GasModule.ino
@@ -12,7 +12,7 @@ FakeRTC rtc;
 
 void setup() {
     // put your setup code here, to run once:
-    Serial.begin(115200);
+    Serial.begin(9600);
     module->setSensor(&sensor);
     module->setStreams(streams);
 #ifdef __AVR__
