0934ba7c128a9203055c64006a374366d6c110f0
Guillaume Pirou (@Elominp) <guillaume.pirou@epitech.eu>
2018-06-19 14:32:05 +0100

    increase buffer of esp answer check to increase stability of response checking

--- src/communication/network/wifi/ESP8266WiFiCommunicator.cpp
+++ src/communication/network/wifi/ESP8266WiFiCommunicator.cpp
@@ -317,9 +317,9 @@ namespace athome {
 
             int ESP8266WiFiCommunicator::_esp_answer_check() {
                 while (!_stream->available());
-                char buffer[8];
-                while (_stream->available()) {
-                    int len = _stream->readBytesUntil(at_nl, buffer, 7);
+                char buffer[16];
+                while (1) {
+                    int len = _stream->readBytesUntil(at_nl, buffer, 15);
                     if (len < 1) {
                         break;
                     }
