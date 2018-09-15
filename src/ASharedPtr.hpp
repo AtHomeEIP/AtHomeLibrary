345f86e297a2a3b8fc9ec7a33b478f1c9de7085a
Guillaume Pirou <guillaume.pirou@epitech.eu>
2018-09-15 19:27:28 +0200

    add smart pointers

---
+++ src/ASharedPtr.hpp
@@ -0,0 +1,40 @@
+#ifndef ASHAREDPTR_HPP_
+#define ASHAREDPTR_HPP_
+#include <map>
+#include "ASmartPtr.hpp"
+
+namespace athome {
+namespace utility {
+namespace memory {
+template <typename T>
+class ASharedPtr : public ASmartPtr<T> {
+ public:
+  ASharedPtr() : ASmartPtr<T>() {}
+
+  ASharedPtr(const ASharedPtr &other) : ASmartPtr<T>(other) {}
+
+  virtual ~ASharedPtr() {}
+
+  long use_count() const { return m_pointers[ASmartPtr<T>::m_value]; }
+
+  bool unique() const { return use_count() == 1; }
+
+ protected:
+  virtual void set(T *ptr) = 0;
+
+  virtual void unset() = 0;
+
+ private:
+  ASharedPtr &operator=(const ASharedPtr &other) {}
+
+ protected:
+  static std::map<void *, size_t> m_pointers;
+};
+
+template <typename T>
+std::map<void *, size_t> ASharedPtr<T>::m_pointers;
+}  // namespace memory
+}  // namespace utility
+}  // namespace athome
+
+#endif  // ASHAREDPTR_HPP_
