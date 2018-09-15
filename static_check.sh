4527b8ab44ca5e1a2e8bb64e0dbd3d577a289b2d
Guillaume Pirou <guillaume.pirou@epitech.eu>
2018-09-15 19:05:36 +0200

    update static_check script

--- static_check.sh
+++ static_check.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cppcheck -I $PWD --enable=all --check-config --force \
-         --suppress=missingIncludeSystem $(find . -name "*.cpp") *.hpp *.h
+         --suppress=missingIncludeSystem $(find src/ -name "*.cpp") \
+         src/*.hpp src/*.h
