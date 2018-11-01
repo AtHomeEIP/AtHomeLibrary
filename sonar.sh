6dc6fac52ed2f988754e516124300ee52d4327fd
Guillaume Pirou <guillaume.pirou@epitech.eu>
2018-11-01 11:00:21 +0100

    add sonar script

---
+++ sonar.sh
@@ -0,0 +1,7 @@
+sonar-scanner \
+-Dsonar.projectKey=tesModulesFramework \
+-Dsonar.organization=elominp-github \
+-Dsonar.sources=. \
+-Dsonar.host.url=https://sonarcloud.io  \
+-Dsonar.login=2ae8b7eb5b9261d855f203212dadb827248f059b \
+-Dsonar.cfamily.build-wrapper-output.bypass=true
